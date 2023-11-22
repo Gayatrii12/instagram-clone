0cffbd17355fee8fdc1d4667c46f49aa1ab2f593
Gayatri Sawant <105240599+Gayatrii12@users.noreply.github.com>
2023-11-22 21:56:24 +0530

    third

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,17 +1,65 @@
 import React from 'react'
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "../styles/Suggestions.css"
 
 function Suggestions() {
   return (
-    <div className='suggestions'>
-      <div className='suggestions__title'>
-        Suggested for you
-      </div>
-      <div className='suggestions__usernames'>
-
+    <div className="suggestions">
+      <div className="suggestions__title">Suggested for you</div>
+      <div className="suggestions__usernames">
+        <div className="sugg_username">
+          <div className="username__left">
+            <AccountCircleIcon className="avatar" />
+            <div className="user__info">
+              <span className="username">realperson</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+        <div className="sugg_username">
+          <div className="username__left">
+            <AccountCircleIcon className="avatar" />
+            <div className="user__info">
+              <span className="username">realperson</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+        <div className="sugg_username">
+          <div className="username__left">
+            <AccountCircleIcon className="avatar" />
+            <div className="user__info">
+              <span className="username">realperson</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+        <div className="sugg_username">
+          <div className="username__left">
+            <AccountCircleIcon className="avatar" />
+            <div className="user__info">
+              <span className="username">realperson</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
+        <div className="sugg_username">
+          <div className="username__left">
+            <AccountCircleIcon className="avatar" />
+            <div className="user__info">
+              <span className="username">realperson</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Suggestions
